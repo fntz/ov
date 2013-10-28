cb9f09967cbf23076198cc4b0c4196e63b7eb78e
fntzr <fantazuor@gmail.com>
2013-10-28 22:24:55 +0300

    add array samples

---
+++ samples/samples.rb
@@ -0,0 +1,36 @@
+require 'ov'
+
+# Array 
+class MyArray 
+  include Ov
+  attr_accessor :arr
+  
+
+  let :initialize do
+    @arr = []
+  end
+
+  let :initialize, Fixnum do |fx|
+    @arr = fx.times.map { nil }
+  end 
+
+  let :initialize, Fixnum, Any do |fx, any|
+    @arr = fx.times.map { any }
+  end
+ 
+end
+
+p MyArray.new()
+p MyArray.new(3)
+p MyArray.new(3, true)
+
+
+
+
+
+
+
+
+
+
+
