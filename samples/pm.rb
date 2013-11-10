790f1c24bb4678c70b82bad20405dd2f644a5ad6
fntzr <fantazuor@gmail.com>
2013-11-10 19:50:13 +0300

    add example

--- samples/pm.rb
+++ samples/pm.rb
@@ -24,12 +24,21 @@ end
 
 include Matching
 
+
+
 match("String", [123]) do 
   try(String, Array) {|str, arr| p "#{str} #{arr}" }
   try(String) {|str| p "#{str}"  }
   otherwise { p "none" }
 end
 
+require "net/http"
+
+match(Net::HTTP.get_response(URI("http://google.com"))) do
+  try(Net::HTTPOK) {|r| p r.header }
+  try(Net::HTTPMovedPermanently) {|r| p r.header }
+  otherwise { p "error" }
+end
 
 
 
